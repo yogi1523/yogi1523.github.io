3f2714318335beff6b4c7089674e09c4fb67a661
yogi1521 <anxamarfer1521g7@gmail.com>
2022-08-18 05:42:08 -0500

    Mi primer sitio web

---
+++ index.js
@@ -0,0 +1,44 @@
+function alerta(){
+    Swal.fire({
+        title:"Mi preparación académica",
+        text:"Tengo conocimientos ya de la carrera gracias a mi bachillerato. En el vi modelado 3D, animación 2D, Stop Motion, además de programación en paginas web, y en Unity.",
+        confirmButtonText:"Cerrar",
+        with: "70%",
+        confirmButtonColor: "#0b6f72",
+        imageUrl: "constancia.jpg"
+
+      })
+}
+function aler(){
+    Swal.fire({
+        title:"Certificación",
+        text:"Tengo bastante conocimiento en Unity, he programado algunos juegos pequeños, conozco muchos términos.",
+        confirmButtonText:"Cerrar",
+        with: "70%",
+        confirmButtonColor: "#0b6f72",
+        imageUrl: "certificado.jpg"
+
+      })
+}
+function funcion(){
+    Swal.fire({
+        title:"Proyecto",
+        text:"Participe en una competición a nivel nacional, esto fue trabajo en equipo, esto me dió experiencia para poder trabajar en equipos y realizar actividades de manera mas eficaz.",
+        confirmButtonText:"Cerrar",
+        with: "70%",
+        confirmButtonColor: "#0b6f72",
+        imageUrl: "reconocimiento.jpg"
+
+      })
+}
+function unity(){
+    Swal.fire({
+        title:"Dibujo",
+        text:"Tengo gran facilidad para realizar dibujos, esto es una habilidad nata, y tambien un hobbie, el cual ayuda a desempeñarme mejor.",
+        confirmButtonText:"Cerrar",
+        with: "70%",
+        confirmButtonColor: "#0b6f72",
+        imageUrl: "dibujo2.jpg"
+
+      })
+}
